005f0bd006bda0de20a17f4bd1e7cb9cd6384dfa
Daniel <dray364@gmail.com>
2017-03-29 16:48:24 -0500

    engagement video was not working / rewrote code to correctly show an auto loop video

--- js/engagementvids.js
+++ js/engagementvids.js
@@ -7,13 +7,12 @@ export default React.createClass({
       <section className="video_section">
         <div className="video_player_container">
           <div className="NBA_video_full_section">
-            <iframe className="NBA_game_highlights"
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/KgsY7ggLmk4&autoplay=1&loop=1&playlist=KgsY7ggLmk4"
-                    frameBorder="1"
-                    allowFullScreen>
-            </iframe>
+            <iframe width="853"
+                    height="480"
+                    src="//www.youtube.com/v/lG5aSZBAuPs&autoplay=1&loop=1&playlist=lG5aSZBAuPs"
+                    frameborder="0"
+                    allowfullscreen></iframe>
+
           </div>
           <h2 className="game_description"> SPURS GAME HIGHLIGHTS
           </h2>

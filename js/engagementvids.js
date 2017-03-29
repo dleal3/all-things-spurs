e949ee0beafb38e24af5e293689b8eb8b2c1f4c6
Daniel <dray364@gmail.com>
2017-03-29 16:59:55 -0500

    changed video all together

--- js/engagementvids.js
+++ js/engagementvids.js
@@ -7,12 +7,12 @@ export default React.createClass({
       <section className="video_section">
         <div className="video_player_container">
           <div className="NBA_video_full_section">
-            <iframe width="853"
-                    height="480"
-                    src="https://www.youtube.com/v/KgsY7ggLmk4&autoplay=1&playlist=KgsY7ggLmk4"
-                    frameBorder="0"
-                    allowFullScreen></iframe>
-
+            <iframe width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/PSz4nabbXWw"
+                    frameborder="0"
+                    allowfullscreen>
+            </iframe>
           </div>
           <h2 className="game_description"> SPURS GAME HIGHLIGHTS
           </h2>
